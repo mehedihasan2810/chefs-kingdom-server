cc6d129fec3b9273fc79fcf9fcc1f95ebc09fd9c
mehedi-hasan <mdmehedihasan2810@gmail.com>
2023-05-01 23:00:03 +0600

    added not found and server error route

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.get("/", (req, res) => {
+  res.send("hey foooo");
+});
 
-app.get('/', (req, res) => {
-    res.send('hey foooo')
-})
+app.use((req, res) => {
+  res.status(404).send({
+    message: "404 Not Found",
+  });
+});
+
+app.use((err, req, res) => {
+  res.status(500).send({
+    error: "Server Error",
+    message: err.message,
+  });
+});
 
 app.listen(PORT, () => {
-    console.log(`server is running at http://localhost:${PORT}`)
-})
+  console.log(`server is running at http://localhost:${PORT}`);
+});
