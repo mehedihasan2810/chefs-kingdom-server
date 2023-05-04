bb5f44c25dcc5363c18515f47dd8f4e84a166de5
mehedi-hasan <mdmehedihasan2810@gmail.com>
2023-05-04 16:03:49 +0600

    added random data in json collection

--- index.js
+++ index.js
@@ -1,17 +1,31 @@
 const express = require("express");
+const cors = require("cors");
+const chefs = require("./data/chefs.json");
+const cuisine = require("./data/cuisine.json");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
-  res.send("hey foooo");
+app.use(express.json());
+app.use(cors());
+
+app.get("/chefs", (req, res) => {
+  res.send(chefs);
 });
 
+app.get("/chefs/:id", (req, res) => {
+  const chefId = req.params.id;
+  const cuisines = cuisine.filter((item) => item.chef_id === Number(chefId));
+  res.send(cuisines);
+});
+
+// not found route
 app.use((req, res) => {
   res.status(404).send({
     message: "404 Not Found",
   });
 });
 
+// server error route
 app.use((err, req, res) => {
   res.status(500).send({
     error: "Server Error",
